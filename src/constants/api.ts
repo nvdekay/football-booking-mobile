167c820009937fd7d19322d6fb9762dd86405e0e
nvdekay <khanhnvd.work@gmail.com>
2026-03-23 23:52:37 +0700

    fix

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,5 @@
-const PROD_URL = 'https://football-booking-api-rp2s.onrender.com'
-// const PROD_URL = 'http://localhost:3000'
+// const PROD_URL = 'https://football-booking-api-rp2s.onrender.com'
+const PROD_URL = 'http://localhost:3000'
 
 
 export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || `${PROD_URL}/api/v1`
